7dfb3704763658450163d0acd32a0584bf02f2ac
zeitrin <zentrox823@gmail.com>
2022-09-29 15:10:26 +0300

    Asisstant

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ print("Ого! Ты выше меня на", height - 0.53, "метра!" )
 print("Мне очень приятно с тобой познакомиться!", name, age, "лет!", "С ростом", height, "метра!")
 actions = int(input("Я могу: \n 1 - рассказать анекдот \n 2 - сложить два целых числа \n 3 - назвать любое число \n"))
 if actions == 1:
-        answer = int(input("1 - анекдот про наркомана \n 2 - анекдот про негра \n"))
+    answer = int(input("1 - анекдот про наркомана \n 2 - анекдот про негра \n"))
         if answer == 1:
-                print("""Нaркомaн приходит в ресторaн. Сaдится зa столик. К нему подходит офицaнт.\n Наркоман: \n — Чем сегодня кормите? \n Офицaнт, открывaя меню: \n — Один момент \n — Тогдa двa тюбикa """)
+            print("""Нaркомaн приходит в ресторaн. Сaдится зa столик. К нему подходит офицaнт.\n Наркоман: \n — Чем сегодня кормите? \n Офицaнт, открывaя меню: \n — Один момент \n — Тогдa двa тюбикa """)
         if answer == 2:
-                print("Заходит негр в бар. На плече у него сидит попугай. Бармен спрашивает: \n — где купил? \n Попугай отвечает: \n — на рынке")
+            print("Заходит негр в бар. На плече у него сидит попугай. Бармен спрашивает: \n — где купил? \n Попугай отвечает: \n — на рынке")
 if actions == 2:
-        number_one = int(input("Введи первое число "))
-        number_two = int(input("Введи второе число "))
-        print(number_one + number_two)
+    number_one = int(input("Введи первое число "))
+    number_two = int(input("Введи второе число "))
+    print(number_one + number_two)
 if actions == 3:
-        rando_number_one = int(input("От "))
-        rando_number_two = int(input("До "))
-        random_result = random.randint(rando_number_one, rando_number_two)
-        print(random_result)
+    rando_number_one = int(input("От "))
+    rando_number_two = int(input("До "))
+    random_result = random.randint(rando_number_one, rando_number_two)
+    print(random_result)
